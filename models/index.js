afbc158899d4a83b0aaca27d257be5f15ef28d4e
deleb <penetra.okulo@gmail.com>
2019-09-07 01:35:40 +0900

    hashtag count update problem

--- models/index.js
+++ models/index.js
@@ -18,7 +18,7 @@ db.sequelize = sequelize; // 인스턴스, sequelize를 모듈화 해서 사용�
 db.post = require("./post")(sequelize, Sequelize);
 db.user = require("./user")(sequelize, Sequelize);
 db.hashtag = require("./hashtag")(sequelize, Sequelize);
-db.posthashtag = require("./posthashtag")(sequelize, Sequelize);
+// db.posthashtag = require("./posthashtag")(sequelize, Sequelize);
 
 /*
 1 대 1 (hasOne, belongsTo)
@@ -35,18 +35,18 @@ db.post.belongsTo(db.user, {
   targetKey: "userId"
 });
 
-db.post.hasMany(db.posthashtag, {
-  foreignKey: 'post_id'
-  // sourceKey: 'id'
-});
+db.post.belongsToMany(db.hashtag, { through: "posthashtag" });
+db.hashtag.belongsToMany(db.post, { through: "posthashtag" });
+//posthashtag 테이블의 hashtag_name 컬럼의 on update cascade 옵션을 제거해야되는데
+// 옵션 조작방법 파악에 실패하여 시간상 직접 posthashtag 모델을 형성하기로 결정
 
-db.posthashtag.belongsTo(db.post, {
-  foreignKey:'post_id',
-  targetKey: 'id'
-});
-  
+// db.post.hasMany(db.posthashtag, {
+//   foreignKey: 'post_id'
+// });
 
-// db.post.belongsToMany(db.hashtag, { through: db.posthashtag });  posthashtag 테이블의 hashtag_name 컬럼의 on update cascade 옵션을 제거해야되는데
-// db.hashtag.belongsToMany(db.post, { through: db.posthashtag });  옵션 조작방법 파악에 실패하여 시간상 직접 posthashtag 모델을 형성하기로 결정
+// db.posthashtag.belongsTo(db.post, {
+//   foreignKey:'post_id',
+//   targetKey: 'id'
+// });
 
 module.exports = db;
--- models/posthashtag.js
+++ models/posthashtag.js
@@ -1,31 +1,38 @@
-module.exports = (sequelize, DataType) => {
-  return sequelize.define(
-    "posthashtag",
-    {
-      post_id: {
-        type: DataType.INTEGER(11),
-        allowNull: false,
-        primaryKey: true,
-        // references: {
-        //   model: 'posts',
-        //   key: 'id',
-        // },
-        onDelete: 'CASCADE',
-        onUpdate: 'CASCADE',
-        allowNull: false
-      },
+// module.exports = (sequelize, DataType) => {
+//   return sequelize.define(
+//     "posthashtag",
+//     {
+//       hashtag_name: {
+//         type: DataType.STRING(30),
+//         onDelete: "CASCADE",
+//         onUpdate: null
+//       }
+//     },
+//     { timestamps: true, underscored: true }
+//   );
+// };
 
-      hashtag_name: {
-        type: DataType.STRING(30),
-        allowNull: false,
-        primaryKey: true,
-        // references: {
-        //   model: 'hashtags',
-        //   key: 'name',
-        // },
-        onDelete: 'CASCADE',
-        allowNull: false
-      },
-    }, { timestamps: true, underscored: true }
-  );
-};
+// post_id: {
+//   type: DataType.INTEGER(11),
+//   allowNull: false,
+//   primaryKey: true,
+//   // references: {
+//   //   model: 'posts',
+//   //   key: 'id',
+//   // },
+//   onDelete: 'CASCADE',
+//   onUpdate: 'CASCADE',
+//   allowNull: false
+// },
+
+// hashtag_name: {
+//   type: DataType.STRING(30),
+//   allowNull: false,
+//   primaryKey: true,
+//   // references: {
+//   //   model: 'hashtags',
+//   //   key: 'name',
+//   // },
+//   onDelete: 'CASCADE',
+//   allowNull: false
+// },
