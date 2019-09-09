7770622cc74f30991e07263051bb15b7db0eb1a9
deleb <penetra.okulo@gmail.com>
2019-09-09 19:39:49 +0900

    refactor post logic for adding user authentication feature of create, update and delete and delete password in payload when login

--- controller/updatePostHashtag.js
+++ controller/updatePostHashtag.js
@@ -1,23 +1,39 @@
 let { user, post, hashtag } = require("../models");
+const jwt = require("jsonwebtoken");
+const secret = process.env.JWT_SECRET;
 
 module.exports = async (req, res) => {
   try {
-    await post.update(
-      {
-        title: req.body.title,
-        subTitle: req.body.subTitle,
-        contents: req.body.contents,
-        category: req.body.category,
-        poster: req.body.poster
-      },
-      {
-        where: { id: req.params.id }
+    const token = req.cookies.access_token;
+    jwt.verify(token, secret, async (err, decoded) => {
+      if (err) {
+        res.status(401).send({
+          success: false,
+          message: "unauthorized"
+        });
+      } else if (Date.now() < decoded.exp) {
+        res.status(401).send({
+          success: false,
+          message: "time out"
+        });
+      } else if (req.body.userId === decoded.userId) {
+        await post.update(
+          {
+            title: req.body.title,
+            subTitle: req.body.subTitle,
+            contents: req.body.contents,
+            category: req.body.category
+          },
+          {
+            where: { id: req.params.id }
+          }
+        );
+        res.status(200).send("Updated");
       }
-    );
-    res.status(200).send("Updated");
+    });
   } catch (err) {
     console.log(err.message);
-    res.status(500).send("server Error");
+    res.status(500).send("Server Error");
   }
 };
 
