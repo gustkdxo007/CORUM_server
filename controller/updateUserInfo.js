e9857ea31e60ebbcdc8fa00a4b49a761101faf8e
Deleb <penetra.okulo@gmail.com>
2019-09-10 13:10:39 +0900

    updateUser is not needed to double authentication

--- controller/updateUserInfo.js
+++ controller/updateUserInfo.js
@@ -18,35 +18,24 @@ module.exports = async (req, res) => {
           message: "time out"
         });
       } else if (req.body.userId === decoded.userId) {
-        let retrievedUser = await user.findOne({
+        let password = undefined;
+        req.body.password ? password = hash(req.body.password) : null;
+        await user.update({
+          name: req.body.name,
+          password: password,
+          nickname: req.body.nickname,
+          gender: req.body.gender,
+          github_addr: req.body.github_addr,
+          contact_email: req.body.contact_email,
+          gitsu: req.body.gitsu,
+          userImage: req.body.userImage,
+          tech: req.body.tech,
+          company: req.body.company,
+          intro: req.body.intro
+        }, {
           where: { userId: req.body.userId }
         });
-
-        if(retrievedUser.dataValues.userId === req.body.userId){
-          let password = undefined;
-          req.body.password ? password = hash(req.body.password) : null;
-          await user.update({
-            name: req.body.name,
-            password: password,
-            nickname: req.body.nickname,
-            gender: req.body.gender,
-            github_addr: req.body.github_addr,
-            contact_email: req.body.contact_email,
-            gitsu: req.body.gitsu,
-            userImage: req.body.userImage,
-            tech: req.body.tech,
-            company: req.body.company,
-            intro: req.body.intro
-          }, {
-            where: { userId: req.body.userId }
-          });
-          res.status(200).json("Success");
-        } else {
-          res.status(401).send({
-            success: false,
-            message: "does not have right to update this user"
-          });
-        }
+        res.status(200).json("Success");
       } else {
         throw new Error("invalid body");
       }
